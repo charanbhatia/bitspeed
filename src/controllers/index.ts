b5c9590af5e205a3a79e45dd22a45b3f656958e9
Charan <charan@example.com>
2026-03-08 19:47:38 +0530

    refactor: add barrel index exports for interfaces, repositories, services, and controllers

---
+++ src/controllers/index.ts
@@ -0,0 +1,1 @@
+export { ContactController } from './contact.controller';
---
+++ src/interfaces/index.ts
@@ -0,0 +1,4 @@
+export { IContact, ICreateContactDTO, IUpdateContactDTO } from './contact.interface';
+export { IIdentifyRequest, IIdentifyResponse, IConsolidatedContact } from './identify.interface';
+export { IContactRepository } from './repository.interface';
+export { IContactService } from './service.interface';
